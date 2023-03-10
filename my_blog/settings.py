85044781aaca3592a00f087aca5d2f658241f8b7
nutan <nutan@xcelvations.com>
2023-03-10 16:49:18 +0530

    Changed database sqllite to mysql

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -75,8 +75,14 @@ WSGI_APPLICATION = 'my_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blogdb',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
