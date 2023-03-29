dc8b39814fa24e637d72ea612a23f1fce7c664bc
nutan <nutan@xcelvations.com>
2023-03-29 23:21:50 +0530

    Created template for article details

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Article
 from django.http import HttpResponse
+from django.http import Http404
 
 # Create your views here.
 def list_of_articles(request):
@@ -9,4 +10,13 @@ def list_of_articles(request):
 
     #return HttpResponse("this is list of articles webpage...")
     return render(request, 'blog/list.html', {'articles': articles})
+    pass
+
+def article_details(request, id):
+    try:
+        article = get_object_or_404(Article, id=id, status=Article.Status.PUBLISHED)
+    except Article.DoesNotExist:
+        raise Http404("No article found.")
+    
+    return render(request, 'blog/detail.html', {'article': article})
     pass
