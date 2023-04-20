3d5744cc33b8bfcb2cdd7912142882450b671bec
nutan <nutan@xcelvations.com>
2023-04-20 19:53:26 +0530

    Created comment system for the article

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.list_of_articles, name='list_of_articles'),
     #path('<int:id>/', views.article_details, name='article_details'),
     path('<int:year>/<int:month>/<int:day>/<slug:article>/', views.article_details, name='article_details'),
+    path('<int:article_id>/comment/', views.comment_for_article, name='comment_for_article'),
 ]
