2abdbfb3a405fc4e217df81bc7a465b6bbc6739b
nutan <nutan@xcelvations.com>
2023-05-04 22:21:22 +0530

    Displayed the most commented articles

--- blog/templatetags/article_tags.py
+++ blog/templatetags/article_tags.py
@@ -1,5 +1,7 @@
 from django import template
 from ..models import Article
+from django.db.models import Count
+
 
 register = template.Library()
 
@@ -12,4 +14,13 @@ def total_articles():
 def show_latest_articles(count=3):
     latest_articles = Article.publishedArticles.order_by('-publish')[:count]
     return {'latest_articles': latest_articles}
+    pass
+
+@register.inclusion_tag('blog/most_commented_articles.html')
+def show_most_commented_articles(count=3):
+    most_commented_articles = Article.publishedArticles.annotate(
+                                    total_comments=Count('comments')
+                                ).order_by('-total_comments')[:count]
+                                
+    return {'most_commented_articles' : most_commented_articles}
     pass
